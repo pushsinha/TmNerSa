6fef3d547ff1da2524b4d303a7990d5efc4ca09b
pushsinha <32235900+pushsinha@users.noreply.github.com>
2018-04-18 17:57:19 -0700

    Update ner_clean_tab.py

--- code/Ner/taggerPy/ner_clean_tab.py
+++ code/Ner/taggerPy/ner_clean_tab.py
@@ -13,8 +13,13 @@ import re
 df1 = pd.DataFrame(columns = ['entity', 'tag'])
 df2 = pd.DataFrame(columns = ['entity', 'tag'])
 nor = 0
+
+# from tagger.py entities are tagged word__tag.
+# first regular expression is to remove all entities with '__O' tags while second is to remove non-alphabets.
 regex1 = re.compile('(.+?)__O')
 regex2 = re.compile('[^a-zA-Z]')
+
+# for comments
 with open("../../ctags.txt", 'r') as file:
     for line in file:
         temp = regex1.sub('', line).split()
@@ -25,6 +30,8 @@ with open("../../ctags.txt", 'r') as file:
             nor = nor + 1
 
 nor = 0
+
+# for articles
 with open("../../atags.txt", 'r') as file:
     for line in file:
         temp = regex1.sub('', line).split()
